a6d61fa920898b4e87600bdfd7f722170f2ec55e
Amosjnr1225 <mwangiamos703@gmail.com>
2025-07-31 18:58:46 +0300

    updated

--- backend/action.js
+++ backend/action.js
@@ -1,9 +1,17 @@
-// app/actions.ts
-"use server";
-import { neon } from "@neondatabase/serverless";
+require("dotenv").config();
 
-export async function getData() {
-    const sql = neon(process.env.DATABASE_URL);
-    const data = await sql`...`;
-    return data;
-}
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
